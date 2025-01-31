ed341b0e5fbebff18899207166483a55be8ae956
Joe O'Pecko <jopecko@users.noreply.github.com>
2025-01-31 11:37:11 -0800

    make argo-workflows usable OOTB
    configures security

--- terraform/infra/argo-workflows.tf
+++ terraform/infra/argo-workflows.tf
@@ -6,9 +6,6 @@ resource "helm_release" "argo_workflows" {
   chart            = "argo-workflows"
   version          = "0.45.4" # v3.6.2
   namespace        = "argo-workflows"
-#  values = [
-#    file("${path.module}/configs/argo-workflows-values.yaml")
-#  ]
 }
 
 data "kubernetes_service" "argo_workflows_server" {
@@ -16,4 +13,57 @@ data "kubernetes_service" "argo_workflows_server" {
     name      = "argo-workflows-server"
     namespace = helm_release.argo_workflows.namespace
   }
+}
+
+resource "kubernetes_role_v1" "argooperator_role" {
+  metadata {
+    name = "argooperatorrole"
+    namespace = helm_release.argo_workflows.namespace
+  }
+
+  rule {
+    api_groups = ["argoproj.io", "", "batch"]
+    resources  = ["*"]
+    verbs      = ["create", "delete", "get", "list", "patch"]
+  }
+}
+
+resource "kubernetes_role_binding" "argooperator_rolebinding" {
+  metadata {
+    name      = "argooperatorrolebinding"
+    namespace = helm_release.argo_workflows.namespace
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = "argooperatorrole"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "argooperator"
+    namespace = helm_release.argo_workflows.namespace
+  }
+}
+
+resource "kubernetes_service_account" "argooperator_sa" {
+  metadata {
+    name = "argooperator"
+    namespace = helm_release.argo_workflows.namespace
+  }
+  secret {
+    name = "argooperator-sa"
+  }
+}
+
+resource "kubernetes_secret" "argooperator_secret" {
+  metadata {
+    annotations = {
+      "kubernetes.io/service-account.name" = kubernetes_service_account.argooperator_sa.metadata.0.name
+    }
+    namespace = helm_release.argo_workflows.namespace
+    name = "argooperator-sa"
+  }
+
+  type                           = "kubernetes.io/service-account-token"
+  wait_for_service_account_token = true
 }
